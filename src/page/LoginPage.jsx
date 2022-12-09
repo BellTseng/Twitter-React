b0ea5af8640b241b2438bf069a7d5d607590557e
21Jasper <pickeng2389002@gmail.com>
2022-12-09 17:39:11 +0800

    Login切版完成，功能尚未實做

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,8 +1,55 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import AuthInput from "../components/AuthInput"
+import './../style/Login.scss'
+import logo from './../image/Icon@2x.jpg'
 
 const LoginPage = () => {
-  return(
-    <h1>LoginPage</h1>
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div
+      className="loginPage"
+    >
+      <img src={logo} alt="title" className="logo" />
+
+      <h3>登入 Alphitter</h3>
+
+      <AuthInput
+        label="帳號"
+        placeholder="請輸入帳號"
+        type="text"
+        value={username}
+        onChange={(nameInputValue) => setUserName(nameInputValue)}
+      />
+
+      <AuthInput
+        label="密碼"
+        placeholder="請輸入密碼"
+        type="password"
+        value={password}
+        onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+      />
+
+      <button
+        className="authButton"
+      >
+        登入
+      </button>
+
+      <div
+        className="linkroute"
+      >
+        <Link to='/signup'>
+          <span>註冊</span>
+        </Link>
+        <span> ● </span>
+        <Link to='/admin/login'>
+          <span>後台登入</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
