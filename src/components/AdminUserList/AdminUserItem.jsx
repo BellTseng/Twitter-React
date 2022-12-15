0f5f0933bdeb402e51479c603b8edb41094119f9
21Jasper <pickeng2389002@gmail.com>
2022-12-15 17:20:54 +0800

    後台使用者清單切版完成

--- src/components/AdminUserList/AdminUserItem.jsx
+++ src/components/AdminUserList/AdminUserItem.jsx
@@ -1,24 +1,49 @@
 import styles from './AdminUserItem.module.scss'
+import tweetCount from './../../image/VectorTweetCount@2x.jpg'
+import tweetLike from './../../image/VectorTweetLike@2x.jpg'
 
-const AdminUserItem = () => {
+const AdminUserItem = ({ user }) => {
   return(
     <div className={styles.userItem}>
       <img 
         className={styles.background}
-        src="https://stickershop.line-scdn.net/stickershop/v1/product/10825585/LINEStorePC/main.png" alt="background" 
+        src={user.background ? user.background : "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989__480.jpg"} 
+        alt="background" 
       />
 
       <img 
         className={styles.avatar}
-        src="https://yt3.googleusercontent.com/ytc/AMLnZu8DV_AUQyPkL9oVUdBIEoIpyKuut4H3VAn9H6iHEQ=s900-c-k-c0x00ffffff-no-rj" 
+        src={user.avatar ? user.avatar : "https://yt3.googleusercontent.com/ytc/AMLnZu8DV_AUQyPkL9oVUdBIEoIpyKuut4H3VAn9H6iHEQ=s900-c-k-c0x00ffffff-no-rj" }
         alt="avatar" 
       />
 
       <div className={styles.userTag}>
-        <p className={styles.userName}>Jas</p>
-        <p className={styles.userAccount}>@ allen789</p>
+        <p className={styles.userName}>{user.userName ? user.userName : 'Jas'}</p>
+        <p className={styles.userAccount}>@{user.account ? user.account : 'ggg123456'}</p>
       </div>
       
+      <div className={styles.userTweetLike}>
+        <div className={styles.tweetCount}>
+          <img
+            src={tweetCount}
+            alt="tweetCount"
+          />
+          <span>{user.tweetCount ? user.tweetCount : '1.5k'}</span>
+        </div>
+
+        <div className={styles.tweetLike}>
+          <img
+            src={tweetLike}
+            alt="tweetLike"
+          />
+          <span>{user.likeCount ? user.likeCount : '20k'}</span>
+        </div>
+      </div>
+
+      <div className={styles.userPopular}>
+        <p className={styles.following}>{user.folloingCount ? user.folloingCount : '59'}個<span>跟隨中</span></p>
+        <p className={styles.follower}>{user.followerCount ? user.followerCount : '230'}位<span>跟隨者</span></p>
+      </div>
       
     </div>
   )
--- src/components/AdminUserList/AdminUserList.jsx
+++ src/components/AdminUserList/AdminUserList.jsx
@@ -1,14 +1,24 @@
 import styles from './AdminUserList.module.scss'
 import AdminUserItem from './AdminUserItem'
 
-const AdminUserList = () => {
+const AdminUserList = ({ userList }) => {
   return (
     <div className={styles.adminUsers}>
       <div className={styles.title}>
         <h4>推文清單</h4>
       </div>
       <div className={`${styles.usersList} ${styles.scrollbar}`}>
-        <AdminUserItem />
+        {
+          userList.map((user) => {
+            return(
+              <AdminUserItem
+                key={user.id}
+                user={user}
+              />
+            )
+          })
+        }
+        
       </div>
     </div>
   )
